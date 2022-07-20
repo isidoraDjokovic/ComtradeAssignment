af8a2326800410f8f1be8a538828c4f9424b0e04
isidoraDjokovic <isidora.djokovic278@gmail.com>
2022-07-20 13:08:41 +0100

    COM-9 logging

--- src/main/java/com/dora/assignment/service/HelloWorldService.java
+++ src/main/java/com/dora/assignment/service/HelloWorldService.java
@@ -1,5 +1,6 @@
 package com.dora.assignment.service;
 
+import com.dora.assignment.aop.Log;
 import com.dora.assignment.entity.HelloWorld;
 import com.dora.assignment.exception.NotFoundException;
 import com.dora.assignment.repository.SpringDataHelloWorldRepository;
@@ -12,19 +13,23 @@ import org.springframework.stereotype.Service;
 public class HelloWorldService {
   @Autowired private final SpringDataHelloWorldRepository repository;
 
+
   public Iterable<HelloWorld> findAll() {
     return repository.findAll();
   }
 
+  @Log
   public void save(HelloWorld helloWorld) {
     repository.save(helloWorld);
   }
 
+  @Log
   public void delete(String languageCode) {
     HelloWorld saved = findByLanguageCode(languageCode);
     repository.delete(saved);
   }
 
+  @Log
   public HelloWorld findByLanguageCode(String languageCode) {
     return repository
         .findByLanguageCode(languageCode)
